527a2ac643332e115155d8578dd95ae3b1171d3f
Lindsay Woods <lindsaywoods@live.ca>
2018-02-09 12:08:19 -0500

    didn/t commit this morning, commented out new code to preserve things that actually work

--- script.js
+++ script.js
@@ -8,40 +8,105 @@
     // include the question?
 // user can go again
 
-const predictions = ["thing", "thing1", "thing2",
+const predictions = [
+"a bird in the sky is a meal in your belly you have yet to catch. now is your chance.", 
 
-"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam deleniti saepe incidunt sunt veritatis molestias mollitia error cumque! Culpa quidem blanditiis necessitatibus ut temporibus!",
+"as you may count on mice running from my magnificence, so you may count on this", 
 
-"quinns adivcs: Lorem ipsum dqolor sit amet consectetur adipisicing elit. Labore possimus optio obcaecati nesciunt illo dolorem.",
+"throw a fish into the ocean and it will swim away.  throw a fish to me and i will eat it.  it is certain.",
 
+"indeed.  keep your claws sharp and soon you shall achieve victory",
+
+"it is as a window in summer. outlook is sunny.",
+
+"sometimes you are the countertop.  sometimes you are the glass and sometimes you are the paw.  dont be the glass.  dont be the countertop.  be the paw.",
+
+"i dont care about this problem.  try again later.  or dont",
+
+"some things are not meant for tiny human brains.  be content with your lack of knowledge.",
+
+"some answers will only come to you in the dead of night.  wait until everyone else in your home is asleep.  race flat out from room to room, careening wildly into furniture.  repeat as necessary.",
+
+"take a nap, then try again",
+
+"you are looking for a mouse in the sky and a bird on the ground.  cease your foolishness.",
+
+"many truths can be found in the entrails of a mouse.  signs point to no.",
+
+"even the most fierce housecat cannot defeat a vaccuum cleaner.  outlook not so good.",
+
+"unruffle your fur and the answer will come to you.",
 
 ]
 
-function getRandomElementFromArray(array){
-    const randomElement = Math.floor(Math.random () * array.length);
-    return array [randomElement];
-    // console.log("hey girl");
+const predictionStyles = [
+    "prediction-1",
+    "prediction-2",
+    "prediction-3",
+    "prediction-4",
+    "prediction-5",
+    "prediction-6",
+    "prediction-7",
+    "prediction-8",
+    "prediction-9",
+    "prediction-10",
+    "prediction-11",
+    "prediction-12",
+    "prediction-13",
+    "prediction-14",
+
+];
+
+function getRandomIndex(array){
+    const randomElement = Math.floor(Math.random() * array.length);
+    return randomElement;
 }
 
 $(function(){
     $("form").on("submit", function(e){
-
         //removing default browser behavior ex refresh
         e.preventDefault();
 
-        
-        let question = $("input").val;
+        // hides form when it gets submitted
+        // const hideform = $("#myForm").hide();
+        // console.log("hide?");
+
+        // 'question' is equal to whatever gets typed into the input
+        let question = $("textarea").val();
+
         // clears the input field
-        $("input").val("");
+        $("textarea").val("");
+
+        
 
         //generates random prediction using reusable function (getRandom...)
-        const getPrediction = getRandomElementFromArray(predictions);
+        const randomIndex = getRandomIndex(predictions);
 
-        if ("input" !== ""){
+        //get prediction text from array 'predictions'
+        const getPrediction = predictions[randomIndex];
+
+        // get prediction style class from array 'predictionStyles'
+        const predictionStyle = predictionStyles[randomIndex];
+        console.log(randomIndex);
+        console.log(question);
+
+        if (question !== ""){
         // puts the prediction on the page
-        $(".results").html(`<p class="prediction"> ${getPrediction}`);
+        
+        // const askagain = $("#myForm").show();
+        // console.log("maybe?");
 
-        }
+        $(".results").html(`
+        <p class = "question">${question}</p>
+        <p class = "prediction ${predictionStyle}"> ${getPrediction}</p>
+        // <p class="askagain ${askagain}"></p>
+        
+        `);
+    }
+
+        
+
+        
         
     });
 
